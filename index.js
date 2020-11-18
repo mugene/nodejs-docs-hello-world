7d86da1d1c367a00882d0bb00e1faa0c40b70e05
Kim,Mugene <mugene_alti@naver.com>
2020-11-18 12:23:13 +0000

    v3

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Bae Suzy!");
 });
-
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3000;
+# const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
